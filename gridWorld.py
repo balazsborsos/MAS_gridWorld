805e29b99d21ab1e994004d8a89cff6a589db293
balazsborsos <b.blazs97@gmail.com>
2020-12-22 21:45:18 +0100

    added giveReward function

--- gridWorld.py
+++ gridWorld.py
@@ -1,6 +1,18 @@
+
+# Init settings
+
 BOARD_ROWS = 9
 BOARD_COLS = 9
 WIN_STATE = (8, 8)
 LOSE_STATE = (6, 5)
 START = (2, 0)
 DETERMINISTIC = True
+
+
+def giveReward(self):
+        if self.state == WIN_STATE:
+            return 50
+        elif self.state == LOSE_STATE:
+            return -50
+        else:
+            return -1
